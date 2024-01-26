0bbd0c79e0bf5c1ae579254c387faad36781a146
chenronglong <raymond2366@outlook.com>
2024-01-26 13:53:50 +0800

    feat: add model 'gpt-4-turbo-preview'

--- app/constant.ts
+++ app/constant.ts
@@ -113,6 +113,15 @@ export const KnowledgeCutOffDate: Record<string, string> = {
 
 export const DEFAULT_MODELS = [
   {
+    name: "gpt-4-turbo-preview",
+    available: true,
+    provider: {
+      id: "openai",
+      providerName: "OpenAI",
+      providerType: "openai",
+    },
+  },
+  {
     name: "gpt-4-1106-preview",
     available: true,
     provider: {
